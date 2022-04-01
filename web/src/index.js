c6102f1da6a8e52555d34d6720de39b1f87ce63c
Misho <michaelbozhilov@gmail.com>
2022-04-02 01:35:33 +0300

    Landing box and merge

--- web/src/index.js
+++ web/src/index.js
@@ -4,8 +4,14 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const generateToken = () => {
-  let charArr = [];
-  return [(Math.random()*charArr.length)];
+  let charArr =  'ABCD12345';
+  let charLen = charArr.length;
+  let result = '';
+  for(var i = 0; i < 5; i++)
+  {
+    result+=charArr.at(Math.random() * (9)); // check for elements out of bounds
+  }
+  return result;
 }
 
 if(localStorage.getItem("user") == null){
