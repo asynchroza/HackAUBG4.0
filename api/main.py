77be91ea73d351d2584e64270695b42f02f4566b
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-04-02 16:13:25 +0300

    get a set of interview questions

--- api/main.py
+++ api/main.py
@@ -1,9 +1,16 @@
-import config
 from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
 import uvicorn
-
 import db
+from utils import *
+from models import *
+
+CORE_QS_PROP = 0.8
+REST_QS_PROP = 0.2
+
+mc = MongoClient(config.MONGO_URI)
+db = mc['mock-interviews']
+
 
 PORT = config.PORT
 HOST = config.HOST
@@ -15,6 +22,33 @@ app = FastAPI()
 async def root():
     db.generate_db()
     return {"message": "meow"}
+
+@app.get('/get-interview/')
+async def get_interview_set(int_req: InterviewRequest):
+    i_url = int_req.str
+    n_q = int_req.number
+    core_qs = int(n_q * CORE_QS_PROP)
+    rest_qs = int(n_q * REST_QS_PROP)
+
+    if isLink(i_url):
+        page_content = downloadText(i_url)
+        role = parseRole(page_content)
+    else:
+        role = i_url
+    tags = ['database', 'general']
+    core_tag = get_core_tag(role)
+    interview_set = []
+
+    for tag in tags:
+        rest_sample = get_random_sample(get_questions_by_tag(tag), rest_qs)
+        for question in rest_sample:
+            interview_set.append(question)
+
+    core_sample = get_random_sample(get_questions_by_tag(core_tag), core_qs)
+    for question in core_sample:
+        interview_set.append(question)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"interview_set": interview_set})
 
 
 @app.post('/login/{token}')
--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,19 @@
 import requests
+import random
+import config
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+mc = MongoClient(config.MONGO_URI)
+db = mc['mock-interviews']
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0'
 }
+
+# check link or string
+def isLink(str):
+    return str.startswith('http')
 
 # Download the URL into data
 def downloadText(url):
@@ -22,3 +32,33 @@ def parseRole(page):
         if tag.has_attr('name'):
             if tag['name'] == 'keywords':
                 return tag['value']
+
+# Get questions from the db by tag
+def get_questions_by_tag(tag):
+    all_questions = db['questions'].find({})
+    questions_by_tag = []
+    for document in all_questions:
+        if tag in document['tags']:
+            questions_by_tag.append(document)
+
+    return questions_by_tag
+
+# Get random sample of the questions
+def get_random_sample(questions, number):
+    question_sample = []
+    indexes = random.sample(range(1, len(questions)), number)
+    for index in indexes:
+        question_sample.append(questions[index]['question'])
+
+    return question_sample
+
+# Get core tag for the role
+def get_core_tag(role):
+    all_tags = db['tags'].find({})
+    tag_names = []
+    for tag in all_tags:
+        if tag['tag'] not in tag_names:
+            tag_names.append(tag['tag'])
+    for tag_name in tag_names:
+        if tag_name.lower() in role.lower():
+            return tag_name
