6e9fef0a7e5fa95e115c2bd3d5ab553da86d79a2
Misho <michaelbozhilov@gmail.com>
2022-04-02 17:37:51 +0300

    Post request

--- web/src/index.js
+++ web/src/index.js
@@ -4,19 +4,40 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
+const axios = require("axios").default;
+
 const generateToken = () => {
   let charArr = "ABCD12345";
   let charLen = charArr.length;
-  let result = "";
-  for (var i = 0; i < 5; i++) {
-    result += charArr.at(Math.random() * 9); // check for elements out of bounds
-  }
+  let result = "1234";
+  // for (var i = 0; i < 5; i++) {
+  //   result += charArr.at(Math.random() * 9); // check for elements out of bounds
+  // }
   return result;
 };
 
 if (localStorage.getItem("user") == null) {
   localStorage.setItem("user", generateToken());
+  axios
+    .post(`http://127.0.0.1:6969/login/`, localStorage.getItem("user"))
+    .then((response) => {
+      console.log(response.status);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .then(() => {
+      console.log("AXIOS POST");
+    });
 }
+
+// axios.get(`http://127.0.0.1:6969/login/${localStorage.getItem("user")}`).then((response)=>{
+//   console.log(response.data);
+// }).catch((error)=>{
+//   console.log(error);
+// }).then(()=>{
+//   console.log("axios passed");
+// })
 
 ReactDOM.render(
   <React.StrictMode>
