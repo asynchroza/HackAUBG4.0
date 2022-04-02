81420efae1b89ed875215903f6c09030cd9be920
Misho <michaelbozhilov@gmail.com>
2022-04-02 19:02:14 +0300

    ChooseMock halfway

--- web/src/Components/ChooseMock.jsx
+++ web/src/Components/ChooseMock.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import Navigation from "./Navbar";
 import styles from "./Styles";
+const axios = require("axios").default;
 
 const ChooseMock = () => {
+  const getLocal = localStorage;
+  const [formState, setFormState] = useState({});
+  const onChange = (event) => {
+    const target = event.target;
+    const value = target.type;
+    const name = target.name;
+
+    formState = {
+      //write some code here to create object to be passed
+    };
+  };
+
+  const handleSubmit = (event) => {
+    axios.post(`http://127.0.0.1:6969/get-interview/`, formState);
+  };
+
   return (
     <div className="container-for-all">
       <Navigation></Navigation>
@@ -16,21 +34,30 @@ const ChooseMock = () => {
                   Paste a link to a LinkedIn job or input the language
                 </Form.Label>
                 <Form.Control
+                  name="link"
                   type="text"
+                  onChange={onChange}
                   placeholder="LinkedIn link, Programming language or the name of the job listing"
                 ></Form.Control>
                 <Form.Group className="group-form-number-qs">
                   <Form.Label>How many questions?</Form.Label>
                   <Form.Control
+                    name="number"
                     type="number"
                     placeholder="We recommend at least 15"
+                    onChange={onChange}
                   ></Form.Control>
                 </Form.Group>
               </Form.Group>
             </Form>
           </div>
         </div>
-        <Button style={styles.customButton} className="btn-generate" href="/mockinterview">
+        <Button
+          style={styles.customButton}
+          type="submit"
+          className="btn-generate"
+          href="/mockinterview"
+        >
           Generate Mokk
         </Button>
       </div>
--- web/src/index.js
+++ web/src/index.js
@@ -9,7 +9,7 @@ const axios = require("axios").default;
 const generateToken = () => {
   let charArr = "ABCD12345";
   let charLen = charArr.length;
-  let result = "1234";
+  let result = "1234"; // changed for testing purposes
   // for (var i = 0; i < 5; i++) {
   //   result += charArr.at(Math.random() * 9); // check for elements out of bounds
   // }
@@ -17,6 +17,7 @@ const generateToken = () => {
 };
 
 if (localStorage.getItem("user") == null) {
+  // if user does not exist, generate user and post to database
   localStorage.setItem("user", generateToken());
   axios
     .post(`http://127.0.0.1:6969/login/${localStorage.getItem("user")}`)
@@ -31,13 +32,6 @@ if (localStorage.getItem("user") == null) {
     });
 }
 
-// axios.get(`http://127.0.0.1:6969/login/${localStorage.getItem("user")}`).then((response)=>{
-//   console.log(response.data);
-// }).catch((error)=>{
-//   console.log(error);
-// }).then(()=>{
-//   console.log("axios passed");
-// })
 
 ReactDOM.render(
   <React.StrictMode>
